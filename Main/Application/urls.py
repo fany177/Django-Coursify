a50804a6d8d61ed3c1cc3f4806b5213e965c2006
Arvind_mocha <arvindarvind2210@gmail.com>
2020-11-14 13:35:42 +0530

    Initial changes

--- Main/Application/urls.py
+++ Main/Application/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('slide9/',views.slide9,name='slide9'),
+    path('slide10/',views.slide10,name='slide10'),
+    path('slide13/',views.slide13,name='slide13'),
+    path('slide14/',views.slide14,name='slide14'),
+    path('slide15/',views.slide15,name='slide15'),
+    path('slide19/',views.slide19,name='slide19'),
+    path('boardexams/',views.boardexams,name='boardexams'),
+    path('schoolexams/',views.schoolexams,name='schoolexams'),
+    path('jeemain/',views.jeemain,name='jeemain'),
 ]
--- Main/Application/views.py
+++ Main/Application/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def slide9(request):
+    return render(request,'Application/Slide9.html')
+
+def slide10(request):
     return render(request,'Application/Slide10.html')
+
+
+def slide13(request):
+    return render(request,'Application/Slide13.html')
+
+def slide14(request):
+    return render(request,'Application/Slide14.html')
+
+def slide15(request):
+    return render(request,'Application/Slide15.html')
+
+
+def slide19(request):
+    return render(request,'Application/Slide19.html')
+
+def boardexams(request):
+    return render(request,'Application/boardexams.html')
+
+def schoolexams(request):
+    return render(request,'Application/schoolexams.html')
+
+
+def jeemain(request):
+    return render(request,'Application/jeemain.html')
