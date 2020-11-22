4bd5222bf660db6790a1f7e642c3c74b63897921
Arvind_mocha <arvindarvind2210@gmail.com>
2020-11-22 12:37:24 +0530

    Initial commit

--- Main/static/main.js
+++ Main/static/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
 	var pathname = window.location.pathname;
 	$('.navbar-nav > li >  a[href="'+pathname+'"]').css({'border-bottom':'.3rem solid #F50057'});
+
+	if(pathname == '/profile/'){
+		$('#section-footer1').hide()
+	}
 })
 
 if($('.navbar-toggler-icon').is(':visible')){
    $('.navbar').addClass('navbar-dark bg-primary')
 }
+
